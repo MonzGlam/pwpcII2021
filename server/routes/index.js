1f87984ce303bee75c31b13ac23cedf49c0f783d
MonzGlam <monz.glam@gmail.com>
2021-04-22 23:39:42 -0500

    Proyecto en estado inicial funcionando

---
+++ server/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author: 'Steve Jobs', appName: 'web browser' });
+});
+
+/*Agregando nueva ruta */
+ router.get('/greeting', function(req, res, next){
+   res.send('Holaaaaaa, estoy en Node JS')
+  })
+ 
+
+ 
+module.exports = router;
